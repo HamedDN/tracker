af72d1736762f636aab7b5266f63f405a92faf4a
HamedDodangi <95305605+HamedDN@users.noreply.github.com>
2024-11-08 14:49:03 +0330

    a

--- public/home.js
+++ public/home.js
@@ -2,16 +2,29 @@ $('#submit').on(
     'click',
     async function(e)
     {
-        const
-            name = $('#name'),
-            data = $('#data')
+        let
+            number = $('#number'),
+            subject = $('#subject'),
+            referral = $('#referraldrop')
         ;
 
-        const newItem = {
-            id: Math.floor(Math.random()),
-            name : name.val(),
-            data : data.val()
-        };
+        if(referral.val() == '')
+        {
+            alert('یک عنوان زا را انتخاب کنید');
+            return;
+        }
+        else
+            if(number.val() == '')
+            {
+                alert('شماره را وارد کنید');
+                return;
+            }
+        else
+            if(subject.val() == '')
+            {
+                alert('موضوع را وارد کنید');
+                return;
+            }
 
         try
         {
@@ -22,15 +35,20 @@ $('#submit').on(
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify(newItem),
+                    body: JSON.stringify({
+                        number: number.val(),
+                        subject : subject.val(),
+                        role : referral.val()
+                    }),
                 }
             );
 
-            if (response.ok)
+            if(response.ok)
             {
-                alert('Item added successfully!');
-                name.val('');
-                data.val('');
+                alert('اطلاعات با موفقیت اضافه شد');
+                number.val('');
+                subject.val('');
+                referral.val('');
             }
         }
         catch (error)
@@ -38,3 +56,27 @@ $('#submit').on(
             console.error('Error adding item:', error);
         }
 });
+
+$('#referraldrop').on(
+    'change',
+    async function(e)
+    {
+        let selectedValue = $(this).val();
+        let response =
+            await fetch(
+                '/api/items',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ role: selectedValue }),
+                }
+            )
+        ;
+        response = await response.json();
+        if(response.length !== 0)
+            $('#referralResult')
+                .text(`گیرنده (${response[0].role}) وضعیت: (${response[0].status}) در تاریخ (${response[0].date})`)
+    }
+);
